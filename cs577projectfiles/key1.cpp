3ebe6b547ae33ee9aee518a4ccbd7849338fcd87
Magister Machinis <30908581+Magister-Machinis@users.noreply.github.com>
2020-10-22 20:52:04 -0400

    key1 seems ok, on to key2

--- cs577projectfiles/key1.cpp
+++ cs577projectfiles/key1.cpp
@@ -1,111 +1,114 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
-#include <algorithm>
+#include "keycommon.h"
 
 using namespace std;
-class stuff
-{
-private:
-	static bool spacecheck(char c) { return(c == ' '); }
-	string nopes = "12353445A342346436345634356B252325523345C266546467786768D684466484444E32423421422412F2342554646464G64687468684";
-	string nopecleaner(bool flag)
-	{
-		char charlist[] = { '1','4','e','2','3','4','s','5','6','7','8','9','y' };
-		string innernopes = this->nopes;
-		for (int p = 0; p <= innernopes.size(); p++)
-		{
-			for (int i = 0; i < sizeof(charlist) / sizeof(charlist[0]); i++)
-			{
-				innernopes.replace(innernopes.begin, string::npos, charlist[i], ' ');
-			}
-		}
 
-		if (flag == true)
-			return totesnopes(innernopes).erase(remove_if(totesnopes(innernopes).begin, totesnopes(innernopes).end, spacecheck), totesnopes(innernopes).end);
-		if (flag == false)
-			return totesyes(innernopes).erase(remove_if(totesyes(innernopes).begin, totesyes(innernopes).end, spacecheck), totesyes(innernopes).end);
-	}
-
-	string totesnopes(string messages)
-	{
-		char charlist[] = { 'E','F','G' };
-		char charlist2[] = { 'A','B','C','D' };
-		char charlist3[] = { 'n','o','p','e' };
-		for (int p = 0; p != this->nopes.end; p++)
-		{
-			for (int i = 0; i < sizeof(charlist) / sizeof(charlist[0]); i++)
-			{
-				messages.replace(messages.begin, string::npos, charlist[i], ' ');
-			}
-		}
-		for (int p = 0; p != this->nopes.end; p++)
-		{
-			for (int i = 0; i < sizeof(charlist2) / sizeof(charlist2[0]); i++)
-			{
-				messages.replace(messages.begin, string::npos, charlist2[i], charlist3[i]);
-			}
-		}
-		return messages;
-	}
-
-	string totesyes(string messages)
-	{
-		char charlist2[] = { 'E','F','G' };
-		char charlist[] = { 'A','B','C','D' };
-		char charlist3[] = { 'y','e','s' };
-		for (int p = 0; p != this->nopes.end; p++)
-		{
-			for (int i = 0; i < sizeof(charlist) / sizeof(charlist[0]); i++)
-			{
-				messages.replace(messages.begin, string::npos, charlist[i], ' ');
-			}
-		}
-		for (int p = 0; p != this->nopes.end; p++)
-		{
-			for (int i = 0; i < sizeof(charlist2) / sizeof(charlist2[0]); i++)
-			{
-				messages.replace(messages.begin, string::npos, charlist2[i], charlist3[i]);
-			}
-		}
-		return messages;
-	}
-public:
-	void nope()
-	{
-		cout << nopecleaner(true);
-		cout << " ";
-	}
-	void nope2()
-	{
-
-		cout << nopecleaner(true);
-	}
-	void sizecheck(int count, string data[])
-	{
-		if (count == 33)
-			this->checkdone(data, 22, 44);
-		if (count < 1)
-			this->nope2();
-		else
-			sizecheck(33, data);
-	}
-	void checkdone(string data[], int use, int less)
-	{
-
-	}
-
-};
-int main(int argsc, string argss[])
+int main(int argc, char *argv[])
 {
 	stuff things;
-	if (argsc > 2)
+	if (argc > 2)
 		things.nope();
-	else if (argsc < 1)
-		things.sizecheck(argsc, argss);
-	else if (argsc > 1)
-		things.sizecheck(argsc, argss);
+	else if (argc < 1)
+		sizecheck(argc, argv);
+	else if (argc > 1)
+		sizecheck(argc, argv);
 
 	return 0;
 }
 
+void sizecheck(int count, char *data[])
+{
+	stuff things;
+	if (count == 33)
+		checkdone(data, 22, 44);
+	if (count < 1)
+		things.nope2();
+	else
+		sizecheck(33, data);
+}
+void checkdone(char *data[], int use, int less)
+{
+	stuff things;
+	
+	if (checker(data[1], '8') == true)
+	{
+		cout << things.nopecleanerpub(false);
+	}
+	else
+	{
+		cout << things.nopecleanerpub(true);
+	}
+}
+bool checker(string data, char check, int count = 0)
+{
+	if (count = -1)
+	{
+		if (data[count + 1] == '/0')
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	switch (data[count])
+	{
+	case '*':
+		return checker(data, 'j', count + 1);
+	case '(':
+		return checker(data, '`', count + 1);
+	case 'h':
+		return checker(data, 't', count + 1);
+	case 't':
+		return checker(data, 'L', count + 1);
+	case 'L':
+		return checker(data, 'S', count + 1);
+	case 'S':
+		return checker(data, 'g', count + 1);
+	case 'g':
+		return checker(data, '7', count + 1);
+	case 'a':
+		return checker(data, 'b', count + 1);
+	case 'b':
+		return checker(data, 'e', count + 1);
+	case 'e':
+		return checker(data, 'c', count + 1);
+	case 'c':
+		return checker(data, 'k', count + 1);
+	case 'k':
+		return checker(data, 'l', count + 1);
+	case 'l':
+		return checker(data, 's', count + 1);
+	case '`':
+		if (count != data.length)
+		{
+			break;
+		}
+		return checker(data, '8', -1);
+	case '9':
+		return checker(data, 'A', count + 1);
+	case 'A':
+		return checker(data, 'a', count + 1);
+	case 's':
+		return checker(data, 'd', count + 1);
+	case 'd':
+		return checker(data, 'h', count + 1);
+	case '8':
+		return checker(data, '.', count + 1);
+	case '.':
+		return checker(data, '1', count + 1);
+	case '1':
+		return checker(data, '9', count + 1);
+	case '7':
+		return checker(data, 'p', count + 1);
+	case 'p':
+		return checker(data, '*', count + 1);
+	default:
+		return false;
+	}
+	return false;
+}
---
+++ cs577projectfiles/keycommon.h
@@ -0,0 +1,93 @@
+#pragma once
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+class stuff
+{
+private:
+	static bool spacecheck(char c) { return(c == ' '); }
+	string nopes = "12353445A342346436345634356B252325523345C266546467786768D684466484444E32423421422412F2342554646464G64687468684";
+	string nopecleaner(bool flag)
+	{
+		char charlist[] = { '1','4','e','2','3','4','s','5','6','7','8','9','y' };
+		string innernopes = this->nopes;
+		for (int p = 0; p <= innernopes.length(); p++)
+		{
+			for (int i = 0; i < sizeof(charlist) / sizeof(charlist[0]); i++)
+			{
+				innernopes.replace(innernopes.begin, string::npos, charlist[i], ' ');
+			}
+		}
+
+		if (flag == true)
+			return totesnopes(innernopes).erase(remove_if(totesnopes(innernopes).begin, totesnopes(innernopes).end, spacecheck), totesnopes(innernopes).end);
+		if (flag == false)
+			return totesyes(innernopes).erase(remove_if(totesyes(innernopes).begin, totesyes(innernopes).end, spacecheck), totesyes(innernopes).end);
+	}
+
+	string totesnopes(string messages)
+	{
+		char charlist[] = { 'E','F','G' };
+		char charlist2[] = { 'A','B','C','D' };
+		char charlist3[] = { 'n','o','p','e' };
+		for (int p = 0; this->nopes[p] != '/0'; p++)
+		{
+			for (int i = 0; i < sizeof(charlist) / sizeof(charlist[0]); i++)
+			{
+				std::replace(messages.begin(), messages.end(), charlist[i], ' ');
+			}
+		}
+		for (int p = 0; this->nopes[p] != '/0'; p++)
+		{
+			for (int i = 0; i < sizeof(charlist2) / sizeof(charlist2[0]); i++)
+			{
+				std::replace(messages.begin(), messages.end(), charlist2[i], charlist3[i]);
+			}
+		}
+		return messages;
+	}
+
+	string totesyes(string messages)
+	{
+		char charlist2[] = { 'E','F','G' };
+		char charlist[] = { 'A','B','C','D' };
+		char charlist3[] = { 'y','e','s' };
+		for (int p = 0; p != this->nopes.end; p++)
+		{
+			for (int i = 0; i < sizeof(charlist) / sizeof(charlist[0]); i++)
+			{
+				std::replace(messages.begin(), messages.end(), charlist[i], ' ');
+			}
+		}
+		for (int p = 0; p != this->nopes.end; p++)
+		{
+			for (int i = 0; i < sizeof(charlist2) / sizeof(charlist2[0]); i++)
+			{
+				std::replace(messages.begin(), messages.end(), charlist2[i], charlist3[i]);
+			}
+		}
+		return messages;
+	}
+public:
+	void nope()
+	{
+		cout << nopecleaner(true);
+		cout << " ";
+	}
+	void nope2()
+	{
+
+		cout << nopecleaner(true);
+	}
+	
+	string nopecleanerpub(bool flag)
+	{
+		nopecleaner(flag);
+	}
+
+};
+
+
